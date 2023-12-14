065ddd7b97d45db9947b1fe02a87c70e7246a3d5
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-12-15 00:06:44 +0900

    feat: 調整

--- CosmosTrigger1/index.ts
+++ CosmosTrigger1/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context } from "@azure/functions"
 
 const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: any[]): Promise<void> {
     if (!!documents && documents.length > 0) {
-        context.log('CosmosTrigger1 Id: ', documents[0].id);
+        context.log('Execute CosmosTrigger1 Id: ', documents[0].id);
     }
 }
 
